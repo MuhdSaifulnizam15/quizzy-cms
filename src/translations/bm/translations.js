8593219df39687672a0de1d1385ddfbda0bb03ad
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-05-11 15:14:37 +0800

    feat: added login page UI

--- src/translations/bm/translations.js
+++ src/translations/bm/translations.js
@@ -1,5 +1,12 @@
 export const TRANSLATIONS_BM = {
     dashboard: 'Laman Utama',
+    email: 'Emel',
+    forgotPassword: 'Lupa Kata Laluan',
     login: 'Login',
+    password: 'Kata Laluan',
     register: 'Daftar',
+    rememberMe: 'Ingat Saya',
+    signIn: 'Log Masuk',
+    signUp: 'Tiada akaun? Sila Daftar di sini',
+    copyright: 'Hak Cipta Terpelihara © ',
 }
--- src/translations/en/translations.js
+++ src/translations/en/translations.js
@@ -1,5 +1,12 @@
 export const TRANSLATIONS_EN = {
     dashboard: 'Dashboard',
+    email: 'Email Address',
+    forgotPassword: 'Forgot password',
     login: 'Login',
+    password: 'Password',
     register: 'Register',
+    rememberMe: 'Remember Me',
+    signIn: 'Sign In',
+    signUp: "Don't have an account? Sign Up Here",
+    copyright: 'All Right Reserved © ',
 }

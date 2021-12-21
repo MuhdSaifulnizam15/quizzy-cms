f15697f8c1140f227917f0dcbcc83950e2690f14
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-12-21 18:02:12 +0800

    feat: initial logic for verify code screen

---
+++ src/pages/Auth/VerifyCode.js
@@ -0,0 +1,13 @@
+import { Icon } from '@iconify/react';
+import { Link as RouterLink } from 'react-router-dom';
+import arrowIosBackFill from '@iconify/icons-eva/arrow-ios-back-fill';
+// material
+import { styled } from '@mui/material/styles';
+import { Box, Button, Link, Container, Typography } from '@mui/material';
+// layouts
+import LogoOnlyLayout from '../../layouts/LogoOnlyLayout';
+// routes
+import { PATH_AUTH } from '../../routes/paths';
+// components
+import Page from '../../components/Page';
+import { VerifyCodeForm } from '../../components/authentication/verify-code';

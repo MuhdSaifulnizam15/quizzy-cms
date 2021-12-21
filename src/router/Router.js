1e4eba43702d8e738ce9c18e995bfe45ce22c078
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-12-21 17:25:13 +0800

    feat: added server error page

--- src/router/Router.js
+++ src/router/Router.js
@@ -14,6 +14,7 @@ import ForgotPassword from '../pages/Auth/ForgotPassword';
 import NotFound from '../pages/Auth/Page404';
 import ComingSoon from '../pages/Auth/ComingSoon';
 import Maintenance from '../pages/Auth/Maintenance';
+import Page500 from '../pages/Auth/Page500';
 
 import DashboardApp from '../pages/DashboardApp';
 import Products from '../pages/Products';
@@ -43,6 +44,7 @@ export default function Router() {
           { path: 'forgot-password', element: <ForgotPassword /> },
           { path: 'coming-soon', element: <ComingSoon /> },
           { path: 'maintenance', element: <Maintenance /> },
+          { path: '500', element: <Page500 /> },
           { path: '/', element: <Navigate to="/dashboard" /> },
           { path: '*', element: <Navigate to="/404" /> }
         ]

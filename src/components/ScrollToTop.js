04c1398a9300be6ccf73a345ee16adc655a13062
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-12-22 14:34:58 +0800

    style: added scrollToTop component

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,14 +1,54 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import arrowIosUpwardOutline from '@iconify/icons-eva/arrow-ios-upward-outline';
+import { Box } from '@mui/system';
 
 // ----------------------------------------------------------------------
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Top: 0 takes us all the way back to the top of the page
+  // Behavior: smooth keeps it smooth!
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, [pathname]);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // Button is displayed after scrolling for 500 pixels
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-  return null;
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  //scroll-to-top classes: fixed, bottom:0, right:0
+  return (
+    <div style={{ position: 'fixed', bottom: 30, right: 30, zIndex: 999 }}>
+      {isVisible && (
+        <Box
+          onClick={scrollToTop}
+          component={Icon}
+          icon={arrowIosUpwardOutline}
+          sx={{ width: 50, height: 50, backgroundColor: '#ABABAB', borderRadius: 10, opacity: 0.7 }}
+        />
+      )}
+    </div>
+  );
 }

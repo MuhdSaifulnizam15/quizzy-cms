275af31f7dccf4b7a52a62ffaa5332514ffa503e
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2022-01-04 12:25:09 +0800

    feat: update api base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Set config defaults when creating the instance
 const axiosInstance = axios.create({
-  baseURL: 'https://api.example.com',
+  baseURL: 'http://localhost:3001/api/v1',
   
 });
 
